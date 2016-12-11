35da737c23afc1980f3368504d4505025b08ea93
fminor <563145721@qq.com>
2016-12-12 00:28:44 +0800

    new

---
+++ DimensionsForMac/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DimensionsForMac
+//
+//  Created by fminor on 10/17/15.
+//  Copyright © 2015 fminor. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ViewController : NSViewController
+
+
+@end
+
---
+++ DimensionsForMac/ViewController.m
@@ -0,0 +1,25 @@
+//
+//  ViewController.m
+//  DimensionsForMac
+//
+//  Created by fminor on 10/17/15.
+//  Copyright © 2015 fminor. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    // Do any additional setup after loading the view.
+}
+
+- (void)setRepresentedObject:(id)representedObject {
+    [super setRepresentedObject:representedObject];
+
+    // Update the view, if already loaded.
+}
+
+@end
